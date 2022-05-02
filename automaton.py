5f368af19483c14d0ab116f11e902d28d63ae01f
iulitaalpetri <92823812+iulitaalpetri@users.noreply.github.com>
2022-05-02 15:20:38 +0300

    Update automaton.py

--- automaton.py
+++ automaton.py
@@ -1,16 +1,94 @@
-class Automaton():
+class Automaton:
 
     def __init__(self, config_file):
         self.config_file = config_file
-        print("Hi, I'm an automaton!")
+
+        self.sigma=[]
+        self.stari=[]
+        self.tranz=[]
+
+        with open(self.config_file) as f:
+
+            linie= f.readline().strip()
+            #sigma
+            while  linie.lower()!= "end":
+
+                if linie[0]=='#' or ("..." in linie) or (":" in linie):
+                    linie = f.readline().strip()
+
+                    continue
+                else:
+                    self.sigma.append(linie)
+                    linie = f.readline().strip()
+
+
+
+
+            #stari
+
+            linie= f.readline().strip()
+
+
+            while linie.lower() != "end":
+
+                if linie[0] == '#' or ("..." in linie) or (":" in linie):
+                    linie = f.readline().strip()
+                    continue
+                else:
+                    tuplu=linie.split(",")
+                    tuplu[0]= tuplu[0].strip()
+                    if len(tuplu)> 1:tuplu[1]= tuplu[1].strip()
+                    self.stari.append(tuplu)
+                    linie = f.readline().strip()
+
+            #tranz
+            linie = f.readline().strip()
+
+            while linie.lower()!="end":
+                if linie[0] == "#" or ("..." in linie) or (":" in linie):
+                    linie = f.readline().strip()
+                    continue
+                else:
+                    x= linie.split(",")
+                    x[0]= x[0].strip()
+                    x[1]= x[1].strip()
+                    x[2]= x[2].strip()
+                    self.tranz.append(x)
+
+                    linie = f.readline().strip()
+
+        print("sigma:",self.sigma)
+        print("tranz:", self.tranz)
+        print(self.stari)
+
+
+
 
     def validate(self):
-        """Return a Boolean
+        # check for unique start state and final state
+        st_init_sau_fin=[t[1] if len(t)> 1 else "" for t in self.stari]
 
-        Returns true if the config file is valid,
-        and raises a ValidationException if the config is invalid.
-        """
-        return "I can't tell if the config file is valid... yet!"
+        if st_init_sau_fin.count("S") > 1:
+            raise Exception("Only one starting state allowed!")
+
+        # check transitions
+        for transition in self.tranz:
+
+            valid_states = [state[0] for state in self.stari]
+
+            if transition[0] not in valid_states or transition[2] not in valid_states or transition[1] not in self.sigma:
+                raise Exception("Transition contains invalid words or states!")
+        return (True, st_init_sau_fin)
+    def retsigma(self):
+        return self.sigma
+    def retstates(self):
+        return self.stari
+    def rettransitions(self):
+        return self.tranz
+    def create_function(self):
+        x= {tuple([tr[0].strip(), tr[1].strip()]): tr[2].strip() for tr in self.tranz}
+
+        return x
 
     def accepts_input(self, input_str):
         """Return a Boolean
@@ -22,13 +100,17 @@ class Automaton():
 
     def read_input(self, input_str):
         """Return the automaton's final configuration
-        
+
         If the input is rejected, the method raises a
         RejectionException.
         """
         pass
-    
+
+
+
 
 if __name__ == "__main__":
-    a = Automaton('your_config_file')
+    a = Automaton('input.txt')
+
     print(a.validate())
+
